2127d39e0f50cc77cb8981d8ce042232a4870c32
Dalton Alleyne <daltondevelop@gmail.com>
2025-08-22 11:23:44 -0400

    Add 'MainMenu' function. Implement in 'Main'.
    Function 'MainMenu' displays options for the program, and returns an integer ID given by the user.
     - 'MainMenu' will loop if an invalid input is given.
    The returned ID tells 'Main' what to do next (ex: 0 = exit).

--- Program.cs
+++ Program.cs
@@ -23,10 +23,75 @@ namespace IRPG_Calculator
             Console.WriteLine(borderLine);
         }
 
+        static int MainMenu()
+        {
+            string response;
+            int choice;
+
+            int[] validChoices = { 0, 1, 2, 3, 4 };
+
+            while (true)
+            {
+                // Display Options
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("  (1) Set Stats");
+                Console.WriteLine("  (2) Optimize AGI");
+                Console.WriteLine("  (3) Simulate Battle");
+                Console.WriteLine("  (4) Estimate Revival Win Rate");
+                Console.WriteLine("  (0) Exit");
+
+                Console.WriteLine("\nSelect your option (0-4):");
+                Console.Write("> ");
+
+                // Get response
+                response = Console.ReadLine();
+
+                try
+                {
+                    // Validate Input
+                    choice = Convert.ToInt32(response);
+
+                    // Break once option is selected
+                    if (validChoices.Contains(choice))
+                    {
+                        break;
+                    }
+                }
+                catch { }
+
+                // Will run if invalid input is given
+                Console.WriteLine("\nInvalid option!");
+                Console.WriteLine("===============================");
+            }
+
+            return choice;
+        }
+
         static void Main(string[] args)
         {
+            int funcID;
+            bool exitProgram = false;
+
+            // Run startup sequence
             Startup();
-            
+
+            // Breaks when funcID = 0
+            while (!exitProgram)
+            {
+                // Get selection
+                funcID = MainMenu();
+
+                // Match selection to function/exit
+                switch (funcID)
+                {
+                    case 0:
+                        exitProgram = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
