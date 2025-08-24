d14cc2b0284fe0aaa47f6899f5d658687849ccd1
Dalton Alleyne <daltondevelop@gmail.com>
2025-08-24 11:02:10 -0400

    MainMenu and SetStatsMenu changes.
    Removed loop from MainMenu.Run in coordination with Program.Main.
    Completed SetStatsMenu.Run; awaiting submenu code completion:
    - SetStatsMenu.NewPlayerStats
    - SetStatsMenu.SavePlayerStats
    - SetStatsMenu.LoadPlayerStats

--- Menu.cs
+++ Menu.cs
@@ -9,19 +9,64 @@ namespace IRPG_Calculator
 
             int[] validChoices = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
-            while (true)
+            // Display Options
+            Console.WriteLine("[Main Menu]\n");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("  (1) Set Stats");
+            Console.WriteLine("  (2) Set Gear");
+            Console.WriteLine("  (3) Set Items");
+            Console.WriteLine("  (4) Optimize AGI to Boss");
+            Console.WriteLine("  (5) Simulate Boss Battle");
+            Console.WriteLine("  (6) Estimate Boss Revival Battle Win Rate");
+            Console.WriteLine("  (7) More Info about Calculator");
+            Console.WriteLine("  (0) Exit");
+
+            Console.WriteLine($"\nSelect your option (0-{validChoices.Length - 1}):");
+            Console.Write("> ");
+
+            // Get response
+            response = Console.ReadLine();
+
+            try
+            {
+                // Validate Input
+                choice = Convert.ToInt32(response);
+
+                // Break once option is selected
+                if (!validChoices.Contains(choice))
+                {
+                    choice = -1;
+                }
+                else
+                {
+                    Program.SeparateSection();
+                }
+            }
+            catch { choice = -1; }
+
+            return choice;
+        }
+    }
+
+    internal static class SetStatsMenu
+    {
+        public static void Run(Character player)
+        {
+            string response;
+            int choice;
+            bool exitSetStatsMenu = false;
+
+            int[] validChoices = { 0, 1, 2, 3 };
+
+            while (!exitSetStatsMenu)
             {
                 // Display Options
-                Console.WriteLine("[Main Menu]\n");
+                Console.WriteLine("[Set Stats Menu]\n");
                 Console.WriteLine("What would you like to do?");
-                Console.WriteLine("  (1) Set Stats");
-                Console.WriteLine("  (2) Set Gear");
-                Console.WriteLine("  (3) Set Items");
-                Console.WriteLine("  (4) Optimize AGI to Boss");
-                Console.WriteLine("  (5) Simulate Boss Battle");
-                Console.WriteLine("  (6) Estimate Boss Revival Battle Win Rate");
-                Console.WriteLine("  (7) More Info about Calculator");
-                Console.WriteLine("  (0) Exit");
+                Console.WriteLine("  (1) New Stats");
+                Console.WriteLine("  (2) Save Stats");
+                Console.WriteLine("  (3) Load Stats");
+                Console.WriteLine("  (0) Main Menu");
 
                 Console.WriteLine($"\nSelect your option (0-{validChoices.Length - 1}):");
                 Console.Write("> ");
@@ -35,26 +80,52 @@ namespace IRPG_Calculator
                     choice = Convert.ToInt32(response);
 
                     // Break once option is selected
-                    if (validChoices.Contains(choice))
+                    if (!validChoices.Contains(choice))
                     {
-                        break;
+                        choice = -1;
+                    }
+                    else
+                    {
+                        Program.SeparateSection();
                     }
                 }
-                catch { }
+                catch { choice = -1; }
 
-                // Will run if invalid input is given
-                Console.WriteLine("\nInvalid option!");
+                switch (choice)
+                {
+                    case 0:
+                        NewPlayerStats(player);
+                        break;
+                    case 1:
+                        SavePlayerStats(player);
+                        break;
+                    case 2:
+                        LoadPlayerStats(player);
+                        break;
+                    case 3:
+                        exitSetStatsMenu = true;
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid option!");
+                        break;
+                }
+                Program.SeparateSection();
             }
-
-            return choice;
         }
-    }
 
-    internal static class SetStatsMenu
-    {
-        public static void Run(Character player)
+        private static void NewPlayerStats(Character player)
         {
-            player.statAtk = 328764720;
+            Program.NotImplementedMessage();
+        }
+
+        private static void SavePlayerStats(Character player)
+        {
+            Program.NotImplementedMessage();
+        }
+
+        private static void LoadPlayerStats(Character player)
+        {
+            Program.NotImplementedMessage();
         }
     }
 }
